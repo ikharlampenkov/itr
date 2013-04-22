a8bc736e25a26c7b1ce24022fcb3aaac4e16a9e4
Ivan Kharlampenkov <ivan87kharlampenkov@gmail.com>
2013-04-22 23:52:41 +0700

    Справочники для проектов

---
+++ application/controllers/ProjectController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Moris
+ * Date: 22.04.13
+ * Time: 23:17
+ * To change this template use File | Settings | File Templates.
+ */
+
+class ProjectController extends Zend_Controller_Action
+{
+    public function init()
+    {
+
+    }
+
+    public function indexAction()
+    {
+
+    }
+
+    public function registerAction()
+    {
+
+    }
+
+    public function developAction()
+    {
+
+    }
+
+}
---
+++ application/controllers/ProjectHandbookController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Moris
+ * Date: 22.04.13
+ * Time: 23:28
+ * To change this template use File | Settings | File Templates.
+ */
+
+class ProjectHandbookController extends Zend_Controller_Action
+{
+    public function init()
+    {
+
+    }
+
+    public function indexAction()
+    {
+
+    }
+
+}
